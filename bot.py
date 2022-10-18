8bf8da83df3a20c880eea114e89db103ea2be2f0
Alvin Lam <alvin.lam29@outlook.com>
2022-10-17 23:59:59 -0500

    updated bot.py - now async

--- bot.py
+++ bot.py
@@ -1,33 +1,28 @@
 # bot.py
-import discord, os, sys
-from discord.ext import commands
+import asyncio
+from email.mime import application
+import discord, os
 from discord.ext.commands import Bot
-if not os.path.isfile("config.py"):
-	sys.exit("'config.py' could not be found. Add the file and try again.")
-else:
-	import config
+from dotenv import load_dotenv
 
-# create bot object, defined the prefix for bot commands, and enable all intents
-bot = Bot(command_prefix=config.BOT_PREFIX, intents=discord.Intents.all())
+load_dotenv()
 
-# # load extensions
-# @bot.command(name="load")
-# async def load(ctx, extension):
-# 	bot.load_extension(f"cos.{extension}")
-
-# # unload extensions
-# @bot.command(name="unload")
-# async def unload(ctx, extension):
-# 	bot.unload_extension(f"cos.{extension}")
+# define intents and create bot object
+intents = discord.Intents.all()
+bot = Bot(command_prefix=os.getenv("BOT_PREFIX"), intents=intents)
 
 # load each of the extensions
-for cog_file in os.listdir('./cogs'):
-	if cog_file.endswith(".py"):
-		try:
-			bot.load_extension(f"cogs.{cog_file[:-3]}")
-			print(f"Loaded {cog_file}")
-		except Exception:
-			print(f"Failed to load {cog_file}")
+async def load():
+	for cog_file in os.listdir('./cogs'):
+		if cog_file.endswith(".py"):
+			try:
+				await bot.load_extension(f"cogs.{cog_file[:-3]}")
+				print(f"Loaded {cog_file}")
+			except Exception:
+				print(f"Failed to load {cog_file}")
 
-# run the bot with token`
-bot.run(config.TOKEN)
+async def main():
+	await load()
+	await bot.start(os.getenv("TOKEN"))
+
+asyncio.run(main())
